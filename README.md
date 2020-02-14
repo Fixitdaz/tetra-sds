4c90186b79a85707cafcf341ee76ee003a2d0fae
Ben Gorham <bgorham@fmgl.com.au>
2020-02-14 17:42:16 +0800

    standards link

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Decoding Tetra Terrestrial Trunked Radio Location Data
 
 Based on the ETSI Standard:
-https://www.etsi.org/deliver/etsi_en/300300_300399/30039202/02.01.01_30/en_30039202v020101v.pdf
+https://www.etsi.org/deliver/etsi_ts/100300_100399/1003921801/01.03.01_60/ts_1003921801v010301p.pdf
 
 ## Overview
 Input a hex string, outputs a dictionary with location information. String can be from a database binary blob or directly from a Tetra Air Interface API.
--- decode.py
+++ decode.py
@@ -3,18 +3,21 @@
 
 
 def hex_to_binary(hex_string):
-    pass
-
-
+    '''Convert hex string to binary string
+    '''
+    # Convert using .format
+    binary_string = "{0:08b}".format(int(hex_string, 16))
+    # Add a 0 to the start (test temp)
+    binary_string = '0'+binary_string
+    return(binary_string)
 
 
 def sds(hex_string):
+    ''' "Main" function, takes a hex string, returns a dictionary with location information.
+    '''
     binary_string = hex_to_binary(hex_string)
     
-
-   
-
-
+    
 
 if __name__ == '__main__':
 
